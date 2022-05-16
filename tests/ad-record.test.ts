2be3ccee28acf2faabd526de6c48d28201cf4cbe
Iwo Matecki <iwo@iwomi.pl>
2022-05-16 08:54:09 +0200

    Add Jest to project and first tests to project

---
+++ tests/ad-record.test.ts
@@ -0,0 +1,27 @@
+import {AdRecord} from "../records/ad.record";
+
+const defaultObj = {
+    name: 'test',
+    description: 'bla',
+    url: 'https://megak.pl',
+    price: 0,
+    lat: 0,
+    lon: 0,
+
+}
+
+test('can build AdRecord', () => {
+    const ad = new AdRecord(defaultObj)
+
+    expect(ad.name).toBe('test');
+    expect(ad.description).toBe('bla');
+})
+
+test('Validates invalid price', () => {
+    expect(()=> new AdRecord({
+        ...defaultObj,
+        price: -3,
+    })).toThrow('Cena nie może być mniejsza niż 0 lub większa niż 9 999 999.');
+})
+
+//@TODO check all validations
